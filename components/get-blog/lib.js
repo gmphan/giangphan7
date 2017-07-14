2b900c6f1b68c003659d53c3aff27ebedd7628ce
Giang Phan <GiangPhan@Clayton.edu>
2017-07-14 16:03:49 -0400

    lib.js in component not includegit add .!

---
+++ components/get-blog/lib.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  console.log('test from component/get-log');
+  $('#includedContent').html('<p>hllo</p>');
+  $("#includedContent").load("/assets/html-in-html.html");
+  $("#btn1").on("click", function(){
+       $("#test1").text("Hello world!");
+   });
+   $("#btn2").click(function(){
+       $("#test2").html("<b>Hello world!</b>");
+   });
+   $("#btn3").click(function(){
+       $("#test3").val("Dolly Duck");
+   });
+});
