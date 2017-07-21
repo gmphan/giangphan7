6fbf5018d968d297960ddc9b39a2a011f4152424
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-07-21 15:27:13 -0400

    working on login to edit

--- view/post-display/lib.js
+++ view/post-display/lib.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
     const $post_content = $("input[name='post_content']");
     $("#postContentDiv").html($post_content.val());
+    /**
+      since the $post_content already have the value,
+      I can out put it in an text area for now,
+      when I click on edit button, I will kick off
+      a click even and have code put the content in to an text area.
+      the text area will be right underneath of the content,
+      after editing, there will be a save button/submit button
+      the data will be send down to blog controller for the time being,
+      I will use alter column to replace the field with the new content.
+      and the page should be refresh by calling an event. 
+    **/
 });
