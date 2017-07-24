5e127b96ade8a1accb5d101c756b48e0633b095e
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-07-24 14:18:52 -0400

    added browser-refresh-taglib

--- view/post-display/lib.js
+++ view/post-display/lib.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     const $post_content = $("input[name='post_content']");
     $("#postContentDiv").html($post_content.val());
+    
     /**
       since the $post_content already have the value,
       I can out put it in an text area for now,
@@ -10,6 +11,6 @@ $(document).ready(function(){
       after editing, there will be a save button/submit button
       the data will be send down to blog controller for the time being,
       I will use alter column to replace the field with the new content.
-      and the page should be refresh by calling an event. 
+      and the page should be refresh by calling an event.
     **/
 });
