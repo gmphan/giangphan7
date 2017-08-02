e7cdbbf80925363e01b8c6fe0d98f1a38c8b4e8b
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-08-02 11:08:45 -0400

    working on editing about

--- controller/slash/index.js
+++ controller/slash/index.js
@@ -1,18 +1,45 @@
 'use strict'
+const Promise = require('promise')
+const mysqlCon = require('~/lib/mysqlCon')
+const conngmp = mysqlCon.gmphanCon()
 
-
-const page = require('~/view/slash/index.marko');
-
-
-
+/***+++++++slashHandler++++++++++++++++*/
+const slashPage = require('~/view/slash/index.marko');
 function slashHandler(req, reply){
-  reply(page.stream())
-
+  queryAbout()
+    .then(function(aboutJson){
+      reply(slashPage.stream(aboutJson))
+    })
 }
 
+const queryAbout=function(){
+  return new Promise(function(resolve,reject){
+    conngmp.query('SELECT * FROM about', function(err, rows){
+      const aboutId=[]
+      const aboutTopic=[]
+      const aboutContent=[]
+      if(err){
+        throw err
+      }else {
+        console.log('Successfully SELECT * FROM about')
+        for(var i=0; i<rows.length; i++){
+          aboutId[i]=rows[i].id
+          aboutContent[i]=rows[i].about_content
+        }
+        const aboutJson={
+          aboutId,
+          aboutContent
+        }
+        const testing = 'testing'
+        resolve(aboutJson)
+      }
+    })
+  })
+}
+/**-------end slashHandler-------------**/
 module.exports=[
   {
-    method:'get',
+    method:'GET',
     path:'/',
     handler:slashHandler
   }
