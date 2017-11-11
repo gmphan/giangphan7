4591c549707114ad692f9a554643c8b574254b21
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-11-11 09:35:44 -0500

    get the modal working on Nav

--- components/sign-in/lib.js
+++ components/sign-in/lib.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
 
-});
+/*
+problem: modal would on same background as main page -> dark and can't be clicking;
+reason: because somewhere in my css has relative or fixed position for Nav or the menu.
+I can move the modal out of the div where it contain that css setting, or remove
+the fixed and relative css, but that will messup the page display.
+
+Best solution:
+Use custom js to change the way it appear to body:
+The below tell the browser, show the modal was it is belong to the <body> and
+not other <div>
+
+$('#myModal').appendTo("body").modal('show');
+Or, if you launch modal using buttons, drop the .modal('show'); and just do:
+
+$('#myModal').appendTo("body")
+This will keep all normal functionality, allowing you to show the modal using a button.
+*/
+
+$(document).ready(function(){
+  $('#signin1').appendTo("body")
+})
