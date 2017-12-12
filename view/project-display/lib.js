97c4bfe2069c3ad9781df9c5d92051063bb47b88
Giang Phan <giangphan@MacbookC02K83E5FFRP.local>
2017-12-12 05:27:28 -0500

    adding task note to each select option

--- view/project-display/lib.js
+++ view/project-display/lib.js
@@ -4,24 +4,32 @@ $(document).ready(function(){
     type:'get',
     url:'/get/task/'+prjId,
     success:function(taskData){
-      //still need data from task table to display
       var sltTsk = document.getElementById('slt_tsk');
-      var option = document.createElement("option");
-        for(let i=0; i<taskData.length; i++){
-          //taskData[i];
-          //'<option value="">'+taskData[i]+'</option>'+
-          option.text = taskData[i].id;
+      const tskId=[];
+        for(var i=0; i<taskData.length; i++){
+          var option = document.createElement("option");
+          option.text = taskData[i].task_name;
           sltTsk.add(option);
-          //console.log(taskData[i].id);
+          tskId[i]=taskData[i].description;
+          //first try to add the below for each task by getElementById.innerHTML
+          /*
+              <div class="row proj-rows">
+                <div class="col-sm-2 proj-left-divs">Activity:</div>
+                <div class="col-sm-8">
+                  <textarea class="form-control" name="activity" rows="5" cols="40"></textarea>
+                </div>
+                <div class="col-sm-2"><button class="btn btn-gphan btn-sm">Add Note</button></div>
+              </div>
+          */
+          //after that I will have to hide the content, and show the content by selecting value.
+          //console.log(tskId[i]);
+          //document.getElementById().innerHTML =
         }
-
-      // for(let i=0; i<taskData.length; i++){
-      //   console.log(taskData[i]);
-      // }
-      //console.log(taskData);
     },
     error:function(){
       alert("Could not get project's tasks to display");
     }
   });
+  var x = document.getElementById("slt_tsk").selectedIndex;
+  console.log(x);
 });
