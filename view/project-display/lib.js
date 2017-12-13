0e3db40f9d5ea1a759b61bbb00e7c0c7723c68ef
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-12-13 11:36:48 -0500

    added work notes box

--- view/project-display/lib.js
+++ view/project-display/lib.js
@@ -23,7 +23,10 @@ $(document).ready(function(){
   });
   $('#slt-tsk').on('change', function() {
       var tskId = this.value;
-      var element  = document.getElementById('tsk_note_textarea');
+      var workNoteElement = document.getElementById('work_note_textarea');
+      var workNoteFrag = document.createDocumentFragment();
+
+      var tskNoteElement  = document.getElementById('tsk_note_textarea');
       var textareaFrag = document.createDocumentFragment();
       //alert(tskId);
       $.ajax({
@@ -31,19 +34,38 @@ $(document).ready(function(){
         url:'/get/task-note/'+tskId,
         success:function(tskNoteData){
           //console.log(tskNoteData);
+          /*create a fragment for work notes*/
+          // var workNoteTextarea = document.createElement('textarea');
+          // workNoteTextarea.className = 'form-control';
+          // workNoteTextarea.name = 'work-notes';
+          // workNoteTextarea.rows = '5';
+          // workNoteTextarea.cols = '40';
+          // workNoteFrag.appendChild(workNoteTextarea);
+          // workNoteElement.appendChild(workNoteFrag);
+
+          document.getElementById('work_note_textarea').innerHTML=
+          '<textarea class="form-control" name="activity" rows="5" cols="40">'+
+          '</textarea>';
+
           for(var i=0; i<tskNoteData.length; i++){
-            // document.getElementById('tsk_note_textarea').innerHTML=
-            // '<textarea class="form-control" name="activity" rows="5" cols="40">'+
-            //   tskNoteData[i].note +
-            // '</textarea>';
+
+            /*
+              create newTextarea element, add class, name and value to that
+              the new textarea element, insert the element into a fragment
+              (textareaFrag) that I created earlier. After all, insert that
+              fragment into an exisiting element (element) that I got earlier
+              from the current exisitng DOM tree
+            */
             var newTextarea = document.createElement('textarea');
             // newTextarea.id = 'sltTskOpt'+taskData[i].id;
             newTextarea.className = 'form-control';
             newTextarea.name='activity';
+            newTextarea.rows="5";
+            newTextarea.cols='40';
             newTextarea.value=tskNoteData[i].note;
             // newTextarea.value = taskData[i].description;
             textareaFrag.appendChild(newTextarea);
-            element.appendChild(textareaFrag);
+            tskNoteElement.appendChild(textareaFrag);
           }
 
         },
