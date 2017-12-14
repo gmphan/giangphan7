e06194025842e17da34fae9f4e88af49bfd3ca19
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-12-14 15:45:08 -0500

    appending note to textarea

--- view/project-display/lib.js
+++ view/project-display/lib.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
         url:'/get/task-note/'+tskId,
         success:function(tskNoteData){
           document.getElementById('work_note_textarea').innerHTML=
-          '<textarea class="form-control" name="work_note" rows="5" cols="40" placeholder="fill out your note here">'+
+          '<textarea class="form-control" name="work_note" rows="5" cols="40" placeholder="Fill out your new note here">'+
           '</textarea>';
           document.getElementById('noteSubmit').innerHTML=
           '<button type="submit" class="btn btn-default btn-sm">Post Note</button>';
@@ -41,42 +41,16 @@ $(document).ready(function(){
             then some how append the content to it because textarea only
             accept string and not for loop
           */
+          for(var i=0; i<tskNoteData.length; i++){
+            var obj=document.getElementById('tsk_note_textarea');
+            //var txt=document.createTextNode(tskNoteData[i].note);
 
-          // document.getElementById('tsk_note_textarea').innerHTML=
-          // '<textarea id="tsk_note_textarea" class="form-control" name="activiy" rows="5" cols="40">'+
-          //   //tskNoteData[1].note;
-          //   for(var i=0; i<tskNoteData.length; i++){
-          //     tskNoteData[i].note;
-          //   }
-          // '</textarea>'
-          // for(var i=0; i<tskNoteData.length; i++){
-          //   document.getElementById('tsk_note_textarea').innerHTML=
-          //   '<textarea id="tsk_note_textarea" class="form-control" name="activiy" rows="5" cols="40" placeholder="activity">'+
-          //     tskNoteData[i].note+
-          //   '</textarea>'
-          //
-          // }
+            obj.append('<p>tskNoteData[i].note</p>');
 
-            /*
-              create newTextarea element, add class, name and value to that
-              the new textarea element, insert the element into a fragment
-              (textareaFrag) that I created earlier. After all, insert that
-              fragment into an exisiting element (element) that I got earlier
-              from the current exisitng DOM tree
-            */
-          // for(var i=0; i<tskNoteData.length; i++){
-          //   var newTextarea = document.createElement('textarea');
-          //   // newTextarea.id = 'sltTskOpt'+taskData[i].id;
-          //   newTextarea.className = 'form-control';
-          //   newTextarea.name='activity';
-          //   newTextarea.rows="5";
-          //   newTextarea.cols='40';
-          //   newTextarea.value=tskNoteData[i].note;
-          //   // newTextarea.value = taskData[i].description;
-          //   textareaFrag.appendChild(newTextarea);
-          //   tskNoteElement.appendChild(textareaFrag);
-          //
-          // }
+            //document.getElementById('tsk_note_textarea').value=tskNoteData[i].note;
+            //document.getElementById('tsk_note_textarea').append=tskNoteData[i].note;
+          }
+
         },
         error:function(){
           alert("couldn't get task note")
