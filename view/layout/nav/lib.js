f25cd7f93ff210aea2031032c8860065dcfa2dad
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2018-01-30 16:34:20 -0500

    working on nav link

--- view/layout/nav/lib.js
+++ view/layout/nav/lib.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function(){
+  console.log(window.location.href);
+  const url = window.location.href;
+  const lastIndexOfUrl = url.substr(url.lastIndexOf('/')+1);
+  console.log(lastIndexOfUrl);
+});
